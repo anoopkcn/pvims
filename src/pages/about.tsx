c0c37465acca73bb18b8b142e1c431b4658189f0
Anoop Chandran <anoopkcn@gmail.com>
2022-08-01 17:27:41 +0200

    Template about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,1 +1,20 @@
-export {}
+import type { NextPage } from 'next'
+import Head from "next/head";
+
+const About: NextPage = () => {
+    return (
+        <div className='h-screen w-screen flex flex-col items-center'>
+            <Head>
+                <title>Solar Materials Vault</title>
+            </Head>
+            <div className="text-center pt-8"> Solar materials vault </div>
+            <div className='p-2' />
+            <div className="overflow-x-auto" >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, nisi cum. Voluptates, beatae ab iusto odit atque quo fuga laudantium iure nulla eius, odio accusamus labore, omnis corporis aliquid inventore?
+            </div>
+            <div className='p-2' />
+        </div>
+    )
+}
+
+export default About
