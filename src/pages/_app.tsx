b432a706a8e40643da4903a84c2cbd2f3c42cf7c
Anoop Chandran <anoopkcn@gmail.com>
2022-08-03 01:19:24 +0200

    Change favicon location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/favicon.ico" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={imageMetaURL} />
@@ -31,22 +31,22 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/favicon/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/favicon/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/favicon/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
       </Head>
     <Component {...pageProps} />
     </div>
